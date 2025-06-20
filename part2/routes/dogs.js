82733356082b1ac48acebab017dabad409d2c4cf
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 16:06:23 +0000

    [automatic commit] 2025-06-20 16:06:23 UTC 122.150.230.127

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     const owner_id = req.session.user.user_id;
     try {
         const [rows] = await db.query (
-            'SELECT dog_id, name, size FROM Dogs WHERE '
-        )
+            'SELECT dog_id, name, size FROM Dogs WHERE owner_id = ?', [owner_id]
+        );
     }
 })
