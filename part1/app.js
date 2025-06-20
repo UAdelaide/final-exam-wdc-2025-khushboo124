f2319e32391476af93db8c331f25d2b3ac55b2bf
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 06:18:52 +0000

    [automatic commit] 2025-06-20 06:18:52 UTC ;; communications error to 208.67.222.222#53: timed out 122.150.230.127

--- part1/app.js
+++ part1/app.js
@@ -71,14 +71,18 @@ app.get('/api/dogs', async (req, res) =>{
     try {
         const [rows] = await pool.execute(`
             SELECT
-            d.name AS dog_name,
-            d.size,
-            u.username AS owner_username
-        FROM
-            Dogs d
-        JOIN
-            Users u ON d.owner_id = u.user_id;
-        `)
+                d.name AS dog_name,
+                d.size,
+                u.username AS owner_username
+            FROM
+                Dogs d
+            JOIN
+                Users u ON d.owner_id = u.user_id;
+        `);
+        res.json(rows);
+
+    } catch (error) {
+        console.
     }
 })
 
