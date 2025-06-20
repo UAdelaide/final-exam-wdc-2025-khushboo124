1f846ad03b9c038ccaf42c602757c808669d78b9
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 06:25:15 +0000

    [automatic commit] 2025-06-20 06:25:15 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -93,7 +93,12 @@ app.get('/api/walkrequests/open', async (req, res) => {
             SELECT
                 wr.request_id,
                 d.name AS dog_name,
-                wr.requested_time`)
+                wr.requested_time,
+                wr.duration_minutes,
+                wr.location,
+                u.username AS owner_username
+            FROM
+                `)
     }
 })
 
