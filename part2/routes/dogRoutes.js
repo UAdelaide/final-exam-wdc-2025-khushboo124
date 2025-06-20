bb7efb521a0fa58e7149db63f071812d5127c942
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 16:19:43 +0000

    [automatic commit] 2025-06-20 16:19:42 UTC 122.150.230.107

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     return res.status(401).json({ error: 'Not logged in'});
    }
    const ownerId = req.session.user.user_id;
-   const [rows] = await db.query
+   const [rows] = await db.query('Select dog_id, name As ')
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' });
   }
