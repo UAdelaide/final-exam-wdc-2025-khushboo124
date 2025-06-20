f58680dfced60605802ba8edd0ee55894c19f94a
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 12:43:31 +0000

    [automatic commit] 2025-06-20 12:43:31 UTC 122.150.230.182

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../models/db');
 
 // GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
