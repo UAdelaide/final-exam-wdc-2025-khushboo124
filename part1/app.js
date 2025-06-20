470f3265afbaccef38d451c44defa782021d6579
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 06:16:04 +0000

    [automatic commit] 2025-06-20 06:16:04 UTC 122.150.230.182

--- part1/app.js
+++ part1/app.js
@@ -64,11 +64,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//API Routes
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
