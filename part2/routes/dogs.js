6370e268c7e74c524e8dd720daec2066f88df8bf
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 16:04:18 +0000

    [automatic commit] 2025-06-20 16:04:18 UTC 122.150.230.107

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/', async)
+router.get('/', async (req, res) => {
+    if(!req.session.user) {
+        return 
+    }
+})
