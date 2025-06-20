02e8ec652fe31b0a152d2ae6058e84d6a08302fd
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 06:25:36 +0000

    [automatic commit] 2025-06-20 06:25:36 UTC 122.150.230.107

--- part1/app.js
+++ part1/app.js
@@ -98,7 +98,9 @@ app.get('/api/walkrequests/open', async (req, res) => {
                 wr.location,
                 u.username AS owner_username
             FROM
-                `)
+                WalkRequests wr
+            JOIN
+                Dogs d ON wr.dog_id = d.dog_id`)
     }
 })
 
