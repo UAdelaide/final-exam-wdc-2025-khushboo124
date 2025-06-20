62cbf344b57287e098add65fdf012a873e0b78a5
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 06:06:42 +0000

    [automatic commit] 2025-06-20 06:06:42 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -13,6 +13,7 @@ var app = express();
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
 const DB_PASSWORD = '';
+const DB_DATABASE = 'DogWalkService';
 
 const pool = mysql.createPool ({
     host: DB_HOST,
