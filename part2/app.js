f7124eaf362f95236eb762582dd25765ce599815
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 16:08:28 +0000

    [automatic commit] 2025-06-20 16:08:28 UTC 122.150.230.244

--- part2/app.js
+++ part2/app.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use('/api/dogs', require('/'))
 app.use(session({
     secret: 'my-key',
     resave: false,
