cb9b38cbf5c6e0e884f0cf3cfccee24911dbba57
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 06:30:48 +0000

    [automatic commit] 2025-06-20 06:30:48 UTC 122.150.230.107

--- part1/app.js
+++ part1/app.js
@@ -120,7 +120,7 @@ app.get('/api/walkers/summary', async (req, res) => {
         COUNT(r.rating_id) AS total_ratings,
         ROUND(AVG(r.rating), 1) AS average_rating,
         COUNT(DISTINCT CASE WHEN wr.status = 'completed' THEN wr.request_id END) AS completed_walks
-        `)
+        FROM`)
 });
 
 
