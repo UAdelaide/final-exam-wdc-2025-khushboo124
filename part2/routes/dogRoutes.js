1205f9b7ca492bbee77129e39d9e409148fa03a6
khushboo124 <a1800662@adelaide.edu.au>
2025-06-20 16:20:45 +0000

    [automatic commit] 2025-06-20 16:20:44 UTC 122.150.230.244

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -11,62 +11,6 @@ router.get('/', async (req, res) => {
    const [rows] = await db.query('Select dog_id, name AS dog_name, size FROM Dogs WHERE owner_id =?', [ownerId]);
    res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch users' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
-
-// POST a new user (simple signup)
-router.post('/register', async (req, res) => {
-  const { username, email, password, role } = req.body;
-
-  try {
-    const [result] = await db.query(`
-      INSERT INTO Users (username, email, password_hash, role)
-      VALUES (?, ?, ?, ?)
-    `, [username, email, password, role]);
-
-    res.status(201).json({ message: 'User registered', user_id: result.insertId });
-  } catch (error) {
-    res.status(500).json({ error: 'Registration failed' });
-  }
-});
-
-router.get('/me', (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({ error: 'Not logged in' });
-  }
-  res.json(req.session.user);
-});
-
-// POST login
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const [rows] = await db.query(`
-      SELECT user_id, username, role FROM Users
-      WHERE username = ? AND password_hash = ?
-    `, [username, password]);
-
-    if (rows.length === 0) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
-
-    //added line to store session
-    req.session.user = rows[0];
-    res.json({ message: 'Login successful', user: rows[0] });
-  } catch (error) {
-    res.status(500).json({ error: 'Login failed' });
-  }
-});
-
-router.post('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Logout Failed' });
-    }
-    res.clearCookie('connect.sid');
-    res.json({ message: 'Logged Out'});
-  });
-});
-module.exports = router;
