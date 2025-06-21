21e83ee31d048b15c95f265f080b54f738cf72b3
khushboo124 <a1800662@adelaide.edu.au>
2025-06-21 00:47:12 +0000

    [automatic commit] 2025-06-21 00:47:12 UTC 122.150.230.182

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
 //     return res.status(401).json({ error: 'Not logged in'});
 //    }
 //    const ownerId = req.session.user.user_id;
-//    const [rows] = await db.query('Select dog_id, name AS dog_name, size FROM Dogs  (removed this line)WHERE owner_id =?', [ownerId]);
+//  (removed this line)WHERE owner_id =?', [ownerId]);
    const [rows] = await db.query('Select dog_id, name AS dog_name, size, owner_id FROM Dogs');
    res.json(rows);
   } catch (error) {
