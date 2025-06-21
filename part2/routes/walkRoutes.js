3d850051044d98fad68a0128c0bfba315a189fff
khushboo124 <a1800662@adelaide.edu.au>
2025-06-21 07:20:26 +0000

    [automatic commit] 2025-06-21 07:20:26 UTC 122.150.230.127

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
       WHERE wr.status = 'open'
-    // `, [ownerId]);
+    `);
     res.json(rows);
   } catch (error) {
     console.error('SQL Error:', error);
