43183cf0038dd51d562bbb40dfe8f84e85b52d43
khushboo124 <a1800662@adelaide.edu.au>
2025-06-21 00:46:52 +0000

    [automatic commit] 2025-06-21 00:46:51 UTC 122.150.230.127

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,10 +5,10 @@ const db = require('../models/db');
 router.get('/', async (req, res) => {
   try {
     //commented to test and display images of dogs on home page
-   if (!req.session.user){
-    return res.status(401).json({ error: 'Not logged in'});
-   }
-   const ownerId = req.session.user.user_id;
+//    if (!req.session.user){
+//     return res.status(401).json({ error: 'Not logged in'});
+//    }
+//    const ownerId = req.session.user.user_id;
 //    const [rows] = await db.query('Select dog_id, name AS dog_name, size FROM Dogs  (removed this line)WHERE owner_id =?', [ownerId]);
    const [rows] = await db.query('Select dog_id, name AS dog_name, size, owner_id FROM Dogs');
    res.json(rows);
