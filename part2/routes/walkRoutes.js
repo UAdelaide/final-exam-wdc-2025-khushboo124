1e70746480b734aa33add4cf802176a8009e5649
khushboo124 <a1800662@adelaide.edu.au>
2025-06-21 07:07:38 +0000

    [automatic commit] 2025-06-21 07:07:38 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -4,6 +4,9 @@ const db = require('../models/db');
 
 // GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
+  if(!req.session.user) {
+    return
+  }
   try {
     const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
